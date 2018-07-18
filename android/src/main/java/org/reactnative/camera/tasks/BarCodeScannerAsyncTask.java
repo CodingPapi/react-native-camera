9e46a5f5ec537689ebd54d482a58d5dc54ef9292
CodingPapi <codingpapi@gmail.com>
2018-07-19 01:48:54 +0800

    add zbar Implementation

--- android/src/main/java/org/reactnative/camera/tasks/BarCodeScannerAsyncTask.java
+++ android/src/main/java/org/reactnative/camera/tasks/BarCodeScannerAsyncTask.java
@@ -5,7 +5,16 @@ import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.PlanarYUVLuminanceSource;
 import com.google.zxing.Result;
-import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.BarcodeFormat;
+// import com.google.zxing.common.HybridBinarizer;
+// import com.google.zxing.common.GlobalHistogramBinarizer;
+import org.reactnative.camera.utils.GlobalHistogramBinarizer;
+import org.reactnative.camera.utils.HybridBinarizer;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.zbar.lib.ZbarManager;
+
+import android.util.Log;
 
 public class BarCodeScannerAsyncTask extends android.os.AsyncTask<Void, Void, Result> {
   private byte[] mImageData;
@@ -13,6 +22,8 @@ public class BarCodeScannerAsyncTask extends android.os.AsyncTask<Void, Void, Re
   private int mHeight;
   private BarCodeScannerAsyncTaskDelegate mDelegate;
   private final MultiFormatReader mMultiFormatReader;
+
+  private final ZbarManager mZarBarManager;
 
   //  note(sjchmiela): From my short research it's ok to ignore rotation of the image.
   public BarCodeScannerAsyncTask(
@@ -27,6 +38,7 @@ public class BarCodeScannerAsyncTask extends android.os.AsyncTask<Void, Void, Re
     mHeight = height;
     mDelegate = delegate;
     mMultiFormatReader = multiFormatReader;
+    mZarBarManager = new ZbarManager();
   }
 
   @Override
@@ -40,27 +52,21 @@ public class BarCodeScannerAsyncTask extends android.os.AsyncTask<Void, Void, Re
     try {
       BinaryBitmap bitmap = generateBitmapFromImageData(mImageData, mWidth, mHeight);
       result = mMultiFormatReader.decodeWithState(bitmap);
-    } catch (NotFoundException e) {
-      BinaryBitmap bitmap = generateBitmapFromImageData(rotateImage(mImageData,mWidth, mHeight),mHeight,mWidth);
-      try {
-        result = mMultiFormatReader.decodeWithState(bitmap);
-      } catch (NotFoundException e1) {
-        //no barcode Found
+      Log.d("codingpapi", "recognize with zxing result:" + result);
+      if (result == null) {
+        result = getQRByZbar(mImageData, mWidth, mHeight);
       }
+    } catch (NotFoundException e) {
+      result = getQRByZbar(mImageData, mWidth, mHeight);
+      // No barcode found, result is already null.
     } catch (Throwable t) {
       t.printStackTrace();
     }
+    if (result != null) {
+      Log.d("codingpapi", "recognize with zxing result:" + result.getText());
+    }
 
     return result;
-  }
-  private byte[] rotateImage(byte[]imageData,int width, int height) {
-    byte[] rotated = new byte[imageData.length];
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        rotated[x * height + height - y - 1] = imageData[x + y * width];
-      }
-    }
-    return rotated;
   }
   @Override
   protected void onPostExecute(Result result) {
@@ -71,17 +77,44 @@ public class BarCodeScannerAsyncTask extends android.os.AsyncTask<Void, Void, Re
     mDelegate.onBarCodeScanningTaskCompleted();
   }
 
-  private BinaryBitmap generateBitmapFromImageData(byte[] imageData, int width, int height) {
+  private Result getQRByZbar(byte[] imageData, int width, int height) {
+    int tX = width / 5;
+    int tY = height / 4;
+    int tXW = tX * 3;
+    int tYW = tY * 2;
+
+    Result result = null;
+    PlanarYUVLuminanceSource source = generateLuminanceSource(imageData, width, height);
+    String stringResult = mZarBarManager.decode(source.getMatrix(), tXW, tYW, false, 0, 0, tXW, tYW);
+    if (stringResult != null) {
+      byte[] fakeYuvData = new byte[10];
+      ResultPoint[] fakePoints = new ResultPoint[10];
+      result = new Result(stringResult, fakeYuvData, fakePoints, BarcodeFormat.QR_CODE);
+    }
+    Log.d("codingpapi", "recognize with zbar result:" + result);
+    return result;
+  }
+
+  private PlanarYUVLuminanceSource generateLuminanceSource(byte[] imageData, int width, int height) {
+    int tX = width / 5;
+    int tY = height / 4;
+    int tXW = tX * 3;
+    int tYW = tY * 2;
     PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(
         imageData, // byte[] yuvData
         width, // int dataWidth
         height, // int dataHeight
-        0, // int left
-        0, // int top
-        width, // int width
-        height, // int height
+        tX, // int left
+        tY, // int top
+        tXW, // int width
+        tYW, // int height
         false // boolean reverseHorizontal
     );
-    return new BinaryBitmap(new HybridBinarizer(source));
+    return source;
+  }
+
+  private BinaryBitmap generateBitmapFromImageData(byte[] imageData, int width, int height) {
+    PlanarYUVLuminanceSource source = generateLuminanceSource(imageData, width, height);
+    return new BinaryBitmap(new GlobalHistogramBinarizer(source));
   }
 }
